13c3dafe71724e491d6d00d10759052d2fba79b7
BingDae <92323245+BingDae@users.noreply.github.com>
2021-11-11 15:47:00 +0900

    업데이트 기능 복구.. (#21)

--- marketKurlyty/src/main/java/first/market/kurlyty/user/service/InfoModifyService.java
+++ marketKurlyty/src/main/java/first/market/kurlyty/user/service/InfoModifyService.java
@@ -3,6 +3,6 @@ package first.market.kurlyty.user.service;
 import first.market.kurlyty.user.vo.UserVO;
 
 public interface InfoModifyService {
-	 UserVO getUserData(UserVO user);
-	 void updateUser(UserVO user);
+	UserVO getUserData(UserVO user);
+	void updateUser(UserVO user);
 }
