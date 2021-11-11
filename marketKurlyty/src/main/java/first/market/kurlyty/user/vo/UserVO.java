5426d0db5292875e4abc8cc5b865b81fcccbb6d2
BingDae <dltmdwns00@naver.com>
2021-11-11 15:42:23 +0900

    업데이트 기능 복구..

--- marketKurlyty/src/main/java/first/market/kurlyty/user/vo/UserVO.java
+++ marketKurlyty/src/main/java/first/market/kurlyty/user/vo/UserVO.java
@@ -14,4 +14,10 @@ public class UserVO {
 	private String user_zipcode;
 	private String user_sex;
 	private String user_birth;
+	
+	//회원가입 수정에 쓸놈들
+	private String user_new_pw;
+	private String user_year;
+	private String user_month;
+	private String user_day;
 }
