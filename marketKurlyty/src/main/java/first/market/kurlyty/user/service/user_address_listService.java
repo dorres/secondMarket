c6b19db619249e22f32a6ceb6774f2ef7f9f337b
bokwang2 <82176709+bokwang2@users.noreply.github.com>
2021-11-17 21:33:21 +0900

    Bk (#30)
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * no message
    
    * 크킄 크밋
    
    * 크크 크밋2
    
    * 커밋

--- marketKurlyty/src/main/java/first/market/kurlyty/user/service/user_address_listService.java
+++ marketKurlyty/src/main/java/first/market/kurlyty/user/service/user_address_listService.java
@@ -8,4 +8,6 @@ import first.market.kurlyty.user.vo.user_address_listVO;
 
 public interface user_address_listService {
 	List<user_address_listVO> getBoard(String user_id) throws Exception;
+	void insert(user_address_listVO vo);
+	void update(user_address_listVO vo);
 }
