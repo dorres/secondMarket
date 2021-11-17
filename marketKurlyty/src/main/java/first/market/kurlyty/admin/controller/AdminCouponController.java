dc2391f77f33f44221705ab0a9b50874aa8e3e1b
BingDae <dltmdwns00@naver.com>
2021-11-17 19:36:15 +0900

    쿠폰15%

--- marketKurlyty/src/main/java/first/market/kurlyty/admin/controller/AdminCouponController.java
+++ marketKurlyty/src/main/java/first/market/kurlyty/admin/controller/AdminCouponController.java
@@ -14,9 +14,17 @@ public class AdminCouponController {
 	@Autowired
 	private AdminCouponService adminCouponService;
 	
-	@RequestMapping("admin_couponpub.mdo") // 쿠폰 발행 페이지
+	@RequestMapping("/admin_couponpub.mdo") // 쿠폰 발행 페이지
 	public String adminCouponPub(AdminCouponVO CouponVo, Model model){
-		return "coupon/admin_couponpub";
+		// DB 쿠폰번호 가져오기
+		AdminCouponVO couponVo = adminCouponService.selectCoupon(CouponVo);
+		
+		String dbCoupon = couponVo.getCoupon_code(); // DB에 있는 쿠폰번호
+		
+		if(CouponVo.getCoupon_code().equals(dbCoupon)) {
+			model.addAttribute("couponData", couponVo);
+			return "mykurly/infoModify2";
+		}
+		return dbCoupon;
 	}
-	
 }// end
--- marketKurlyty/src/main/java/first/market/kurlyty/admin/dao/AdminCouponDAO.java
+++ marketKurlyty/src/main/java/first/market/kurlyty/admin/dao/AdminCouponDAO.java
@@ -5,13 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import first.market.kurlyty.admin.vo.AdminCouponVO;
+import first.market.kurlyty.user.vo.UserVO;
 
 @Repository
 public class AdminCouponDAO {
 	@Autowired
 	private SqlSessionTemplate sqlSession;
 	
-	public AdminCouponVO insertCoupon(AdminCouponVO couponVo) {
-		return couponVo;
+	public void insertCoupon(AdminCouponVO couponVo) {
+		sqlSession.insert("AdminCouponDAO.insertCoupon", couponVo);
+		
+	}
+
+	public AdminCouponVO getcoupon(AdminCouponVO couponVo) {
+		return sqlSession.selectOne("AdminCouponDAO.getcoupon", couponVo);
 	}
 }
